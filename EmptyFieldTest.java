d2f479366f564dccb9355361a2c5baaed3621769
Nipunirangika <139099234+Nipunirangika@users.noreply.github.com>
2025-03-20 20:39:37 +0530

    Add files via upload

---
+++ EmptyFieldTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class EmptyFieldTest {
+    @Test
+    public void fblogin () {
+        // Set the path to ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "D:\\softwares\\intelij\\Driver\\chromedriver.exe");
+        // Create a new Chrome WebDriver instance
+        WebDriver driver = new ChromeDriver();
+        // Open Facebook login page
+        driver.get("https://www.facebook.com/login");
+        // Find username and password field and enter username,password there
+        driver.findElement(By.id("email")).sendKeys("");
+        driver.findElement(By.id("pass")).sendKeys("");
+        // Find login button and click it
+        driver.findElement(By.name("login")).click();
+        // Print current URL
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        // Close the browser window
+        //driver.quit();
+    }}
+
